ce8b039eb3415ab3d8e19187ca3377abc8165c30
pepemikrobi <robert.slaski@gmail.com>
2024-03-21 11:33:18 +0100

    typo

--- Part 4 - IaC for SDN Controllers/Lab 4.3. Managing Controllers with Terraform/Task 9 Building scalable Terraform code using modules/tenant_policy.tf
+++ Part 4 - IaC for SDN Controllers/Lab 4.3. Managing Controllers with Terraform/Task 9 Building scalable Terraform code using modules/tenant_policy.tf
@@ -44,22 +44,22 @@ resource "aci_subnet" "Subnets" {
     scope = ["public"]
 }
 
-resource "aci_application_profile" "APP1" {
-    name = "APP1"
-    tenant_dn = aci_tenant.PODX_Tenant1.id
-}
+# resource "aci_application_profile" "APP1" {
+#     name = "APP1"
+#     tenant_dn = aci_tenant.PODX_Tenant1.id
+# }
 
-resource "aci_application_epg" "EPG1" {
-    name = "EPG1"
-    application_profile_dn = aci_application_profile.APP1.id
-    relation_fv_rs_bd = aci_bridge_domain.BDs["BD1"].id
-}
+# resource "aci_application_epg" "EPG1" {
+#     name = "EPG1"
+#     application_profile_dn = aci_application_profile.APP1.id
+#     relation_fv_rs_bd = aci_bridge_domain.BDs["BD1"].id
+# }
 
-resource "aci_application_epg" "EPG2" {
-    name = "EPG2"
-    application_profile_dn = aci_application_profile.APP1.id
-    relation_fv_rs_bd = aci_bridge_domain.BDs["BD2"].id
-}
+# resource "aci_application_epg" "EPG2" {
+#     name = "EPG2"
+#     application_profile_dn = aci_application_profile.APP1.id
+#     relation_fv_rs_bd = aci_bridge_domain.BDs["BD2"].id
+# }
 
 module "APP1_EPG1" {
     source = "./modules/app_epg"
