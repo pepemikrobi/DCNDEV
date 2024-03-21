99c3a9d64e0df41ff37a255c09d10e577ddb8181
pepemikrobi <robert.slaski@gmail.com>
2024-03-21 09:21:29 +0100

    L4.3T5 .init.sh anonymized

--- Part 4 - IaC for SDN Controllers/Lab 4.3. Managing Controllers with Terraform/Task 5 Prepare Terraform environment for ACI/.init.sh
+++ Part 4 - IaC for SDN Controllers/Lab 4.3. Managing Controllers with Terraform/Task 5 Prepare Terraform environment for ACI/.init.sh
@@ -1,9 +1,9 @@
 export GITLAB_ACCESS_TOKEN=glpat-<your_gitlab_token>
 export TF_STATE_NAME=aci
 terraform init \
-    -backend-config="address=https://podX-mgmt.sdn.lab/api/v4/projects/3/terraform/state/$TF_STATE_NAME" \
-    -backend-config="lock_address=https://podX-mgmt.sdn.lab/api/v4/projects/3/terraform/state/$TF_STATE_NAME/lock" \
-    -backend-config="unlock_address=https://podX-mgmt.sdn.lab/api/v4/projects/3/terraform/state/$TF_STATE_NAME/lock" \
+    -backend-config="address=https://podX-mgmt.sdn.lab/api/v4/projects/<ProjectID>/terraform/state/$TF_STATE_NAME" \
+    -backend-config="lock_address=https://podX-mgmt.sdn.lab/api/v4/projects/<ProjectID>/terraform/state/$TF_STATE_NAME/lock" \
+    -backend-config="unlock_address=https://podX-mgmt.sdn.lab/api/v4/projects/<ProjectID>/terraform/state/$TF_STATE_NAME/lock" \
     -backend-config="username=podX" \
     -backend-config="password=$GITLAB_ACCESS_TOKEN" \
     -backend-config="lock_method=POST" \
