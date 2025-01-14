79490965bc1bbb3e65abdb7d8c6c2255d969e8fe
pepemikrobi <robert.slaski@gmail.com>
2025-01-14 23:38:48 +0100

    Lab 6.3 multiple updates till T5

--- Part 6 - Developing CI CD Pipeline Automation/Lab 6.3. Creating an Automated Network Pipeline/Task 4 Prepare the CICD Terraform deployment environment for ACI/.init.sh
+++ Part 6 - Developing CI CD Pipeline Automation/Lab 6.3. Creating an Automated Network Pipeline/Task 4 Prepare the CICD Terraform deployment environment for ACI/.init.sh
@@ -1,10 +1,10 @@
 export GITLAB_ACCESS_TOKEN=${GITLAB_ACCESS_TOKEN}
-export TF_STATE_NAME=${TF_STATE_NAME_ACI}
+export TF_STATE_NAME=${TF_STATE_FILE_ACI}
 terraform init \
     -backend-config="address=https://podX-mgmt.sdn.lab/api/v4/projects/${CI_PROJECT_ID}/terraform/state/$TF_STATE_NAME" \
     -backend-config="lock_address=https://podX-mgmt.sdn.lab/api/v4/projects/${CI_PROJECT_ID}/terraform/state/$TF_STATE_NAME/lock" \
     -backend-config="unlock_address=https://podX-mgmt.sdn.lab/api/v4/projects/${CI_PROJECT_ID}/terraform/state/$TF_STATE_NAME/lock" \
-    -backend-config=username=${TF_USERNAME} \
+    -backend-config=username=${GITLAB_USERNAME} \
     -backend-config=password=${GITLAB_ACCESS_TOKEN} \
     -backend-config="lock_method=POST" \
     -backend-config="unlock_method=DELETE" \
