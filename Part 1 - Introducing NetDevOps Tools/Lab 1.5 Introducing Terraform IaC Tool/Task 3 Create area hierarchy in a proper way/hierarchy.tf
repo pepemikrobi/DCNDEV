d29c8ec5e7a17cae0d8ce5f252ea9e5af9f6faf8
pepemikrobi <robert.slaski@gmail.com>
2026-04-27 22:20:46 +0200

    Added PODX area to the hierarchy

--- Part 1 - Introducing NetDevOps Tools/Lab 1.5 Introducing Terraform IaC Tool/Task 3 Create area hierarchy in a proper way/hierarchy.tf
+++ Part 1 - Introducing NetDevOps Tools/Lab 1.5 Introducing Terraform IaC Tool/Task 3 Create area hierarchy in a proper way/hierarchy.tf
@@ -1,9 +1,9 @@
-resource "dnacenter_area" "europe" {
+resource "dnacenter_area" "podX" {
     provider = dnacenter
     parameters {
         site {
             area {
-            name        = "Europe"
+            name        = "PODX"
             parent_name = "Global"
             }
         }
@@ -11,13 +11,27 @@ resource "dnacenter_area" "europe" {
     }
 }
 
-    resource "dnacenter_area" "poland" {
+resource "dnacenter_area" "europe" {
+    provider = dnacenter
+    parameters {
+        site {
+            area {
+            name        = "Europe"
+            parent_name = "Global/PODX"
+            }
+        }
+        type = "area"
+    }
+    depends_on = [ dnacenter_area.podX ]
+}
+
+resource "dnacenter_area" "poland" {
     provider = dnacenter
     parameters {
         site {
             area {
             name        = "Poland"
-            parent_name = "Global/Europe"
+            parent_name = "Global/PODX/Europe"
             }
         }
     type = "area"
