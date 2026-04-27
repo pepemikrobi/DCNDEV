56f58fdfb2ded9bd0026700c85d5d24a8a2c8d7a
pepemikrobi <robert.slaski@gmail.com>
2026-04-27 21:35:25 +0200

    New area hierarchy for common CatC

--- Part 1 - Introducing NetDevOps Tools/Lab 1.5 Introducing Terraform IaC Tool/Task 2 Create area hierarchy (an improper way)/hierarchy (final).tf
+++ Part 1 - Introducing NetDevOps Tools/Lab 1.5 Introducing Terraform IaC Tool/Task 2 Create area hierarchy (an improper way)/hierarchy (final).tf
@@ -1,9 +1,9 @@
-resource "dnacenter_area" "europe" {
+resource "dnacenter_area" "pod5" {
     provider = dnacenter
     parameters {
         site {
             area {
-            name        = "Europe"
+            name        = "POD5"
             parent_name = "Global"
             }
         }
@@ -11,7 +11,20 @@ resource "dnacenter_area" "europe" {
     }
 }
 
-    resource "dnacenter_area" "poland" {
+resource "dnacenter_area" "europe" {
+    provider = dnacenter
+    parameters {
+        site {
+            area {
+            name        = "Europe"
+            parent_name = "POD5"
+            }
+        }
+        type = "area"
+    }
+}
+
+resource "dnacenter_area" "poland" {
     provider = dnacenter
     parameters {
         site {
