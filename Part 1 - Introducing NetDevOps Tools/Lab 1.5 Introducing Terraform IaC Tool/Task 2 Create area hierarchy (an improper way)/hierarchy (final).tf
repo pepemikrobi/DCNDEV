6181072021a7253b607a705b7c25d4903e8fa447
pepemikrobi <robert.slaski@gmail.com>
2026-06-03 18:04:15 +0200

    PODX added

--- Part 1 - Introducing NetDevOps Tools/Lab 1.5 Introducing Terraform IaC Tool/Task 2 Create area hierarchy (an improper way)/hierarchy (final).tf
+++ Part 1 - Introducing NetDevOps Tools/Lab 1.5 Introducing Terraform IaC Tool/Task 2 Create area hierarchy (an improper way)/hierarchy (final).tf
@@ -1,10 +1,23 @@
+resource "dnacenter_area" "podX" {
+  provider = dnacenter
+  parameters {
+    site {
+      area {
+        name        = "PODX"
+        parent_name = "Global"
+      }
+    }
+    type = "area"
+  }
+}
+
 resource "dnacenter_area" "europe" {
     provider = dnacenter
     parameters {
         site {
             area {
             name        = "Europe"
-            parent_name = "Global"
+            parent_name = "Global/PODX"
             }
         }
         type = "area"
@@ -17,7 +30,7 @@ resource "dnacenter_area" "europe" {
         site {
             area {
             name        = "Poland"
-            parent_name = "Global/Europe"
+            parent_name = "Global/PODX/Europe"
             }
         }
     type = "area"
